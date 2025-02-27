cf7537c138e163606414804a82e96bc2b724f2a7
Gabriel Ferreira <gf-castro2012@hotmail.com>
2025-02-27 11:36:45 -0300

    :sparkles: feat: Adicionando readme do desafio

--- Sprint 1/Exercicios/ex8.sql
+++ Sprint 1/Exercicios/ex8.sql
@@ -1,7 +1,8 @@
 select t.cdvdd,t.nmvdd 
 from tbvendedor t 
 left join tbvendas v 
-on v.cdvdd = t.cdvdd 
+on v.cdvdd = t.cdvdd
+where status = 'Concluído' 
 group by t.nmvdd 
 order by count(v.cdven) DESC 
 limit 1;
